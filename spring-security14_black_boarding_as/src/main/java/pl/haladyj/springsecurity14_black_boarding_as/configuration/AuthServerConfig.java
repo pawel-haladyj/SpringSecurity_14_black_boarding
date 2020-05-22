f7d26a497b2075b85708ae5d5b96e6a5a6be95b6
Paweł Haładyj <pawel@haladyj.pl>
2020-05-22 08:59:52 +0200

    establiished AuthServer

---
+++ spring-security14_black_boarding_as/src/main/java/pl/haladyj/springsecurity14_black_boarding_as/configuration/AuthServerConfig.java
@@ -0,0 +1,51 @@
+package pl.haladyj.springsecurity14_black_boarding_as.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("client1")
+                .secret("secret1")
+                .scopes("read")
+                .authorizedGrantTypes("password", "refresh_token")
+                .and()
+                .withClient("resourceserver")
+                .secret("12345");
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.userDetailsService(userDetailsService);
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
